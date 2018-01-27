801bfd799e4fae0301b8674cf5b32f1d2bc7380b
webelf000 <steven.jung86000@gmail.com>
2018-01-27 04:15:59 -0800

    add mobile side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </React.Fragment>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </React.Fragment>
+    )
+  }
+}
 
 export default Layout
