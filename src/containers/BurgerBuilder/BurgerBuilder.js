38cb0dc16c8eca76575e273a1a11c2489fe9685e
webelf000 <steven.jung86000@gmail.com>
2018-01-26 20:46:12 -0800

    add burgerbuild controller layout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Burger from '../../components/Burger/Burger'
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -13,8 +14,8 @@ class BurgerBuilder extends Component {
   render() {
     return (
       <React.Fragment>
-        <Burger ingredients={this.state.ingredients}/>
-        <div>Build Control</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </React.Fragment>
     );
   }
