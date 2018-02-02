329e2f11c851e94dc9524ed5bcd96667d66cba7a
webelf000 <steven.jung86000@gmail.com>
2018-02-02 09:47:25 -0800

    persistent Auth state with localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,8 +8,13 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componentWillMount = () => {
+    this.props.onTryAutoSignUp();
+  }
+  
   render() {
     return (
       <div>
@@ -24,4 +30,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignUp: () => dispatch(actions.authCheckState())
+  };
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
